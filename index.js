752d5db27089188a0b587a0acdc5354ed0afefe2
Marc Steele <steelegbr@gmail.com>
2020-02-28 22:09:48 +0000

    Got the sagas and inital config load working.

--- index.js
+++ index.js
@@ -4,18 +4,12 @@
 
 import * as React from 'react';
 import { AppRegistry } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { Provider as StoreProvider } from 'react-redux';
 import App from './App';
 import {name as appName} from './app.json';
 
 export default function Main() {
     return (
-      <StoreProvider>
-        <PaperProvider>
-            <App />
-        </PaperProvider>
-      </StoreProvider>
+      <App />
     );
   }
 

2d94286cfd0cab9130ae20a26661548f6a09a686
Marc Steele <marcsteele@Hamish.local>
2020-02-29 11:11:43 +0000

    Added initial error logging and station load logic.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import * as React from 'react';
 import { AppRegistry } from 'react-native';
+import '@react-native-firebase/crashlytics';
 import App from './App';
 import {name as appName} from './app.json';
 

406475f836c086067f5020793f6c7fcb0f252791
Marc Steele <marcsteele@Hamish.local>
2020-03-03 18:05:02 +0000

    App renamed to Solid Radio.

--- ios/SolidRadioTests/SolidRadioTests.m
+++ ios/SolidRadioTests/SolidRadioTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SolidRadioMobileTests : XCTestCase
+@interface SolidRadioTests : XCTestCase
 
 @end
 
-@implementation SolidRadioMobileTests
+@implementation SolidRadioTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {

36005f5c5a5791c093e5dbbda4bc849783f1b314
Marc Steele <marcsteele@Hamish.local>
2020-05-05 06:45:23 +0100

    Split the header into its own component.

--- components/PlayerOverlay.js
+++ components/PlayerOverlay.js
@@ -8,6 +8,7 @@ import Animated from 'react-native-reanimated';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import PlayerCarousel from './PlayerCarousel';
 import AdComponent from './AdComponent';
+import OverlayHeaderComponent from './OverlayHeaderComponent';
 
 class PlayerOverlay extends Component {
 
@@ -41,52 +42,10 @@ class PlayerOverlay extends Component {
 
     renderHeader = () => {
 
-        const { styles, theme, fall, header } = this.props;
-
-        const animatedHeaderContentOpacity = Animated.interpolate(fall, {
-            inputRange: [0.75, 1],
-            outputRange: [0, 1],
-            extrapolate: Animated.Extrapolate.CLAMP,
-        });
+        const { fall } = this.props;
 
         return(
-            <View style={styles.headerContainer}>
-                <Animated.View
-                    style={[
-                        styles.headerSubContainer,
-                        {
-                            opacity: animatedHeaderContentOpacity
-                        }
-                    ]}
-                >
-                    <TouchableWithoutFeedback
-                        style={styles.headerImageWrapper}
-                        onPress={this.onHeaderPress}
-                    >
-                        <Image
-                            source={{ uri: header.artUrl }}
-                            style={styles.headerImage}
-                        />
-                    </TouchableWithoutFeedback>
-                    <View style={styles.headerText}>
-                        <Text theme={theme}>{header.title}</Text>
-                        <Caption theme={theme}>
-                            {header.subtitle}
-                        </Caption>
-                    </View>
-                    <View style={styles.headerButton}>
-                        <Button
-                            icon="cast"
-                            theme={theme}
-                            
-                        />
-                        <Button
-                            icon="play"
-                            theme={theme}
-                        />
-                    </View>
-                </Animated.View>
-            </View>
+            <OverlayHeaderComponent fall={fall} onHeaderPress={this.onHeaderPress} />
         );
     }
 
@@ -115,22 +74,9 @@ class PlayerOverlay extends Component {
 
 const mapStateToProps = state => {
 
-    // Work out if and what we should be displaying onscreen
+    // Work out if we should be displaying onscreen
 
     const showOnscreen = state.player.playlist.length > 0;
-    let header = { title: '', subtitle: '', artUrl: '' }
-    const currentItem = state.player.playlist[state.player.currentItem];
-
-    if (currentItem !== undefined && currentItem.type == 'station') {
-        const station = state.stations[currentItem.name]
-        header.title = `${station.name} - ${station.onAir.show}`;
-        header.subtitle = `${station.nowPlaying.artist} - ${station.nowPlaying.title}`;
-        if (station.nowPlaying.artUrl) {
-            header.artUrl = station.nowPlaying.artUrl;
-        } else {
-            header.artUrl = station.onAir.image;
-        }
-    }
 
     // Calculate dynamic sizing
 
@@ -145,38 +91,9 @@ const mapStateToProps = state => {
 
     return {
         showOnscreen: showOnscreen,
-        header: header,
         theme: state.theme,
         snapPoints: [headerHeight, Dimensions.get("window").height - headerHeight - 95],
         styles: {
-            headerImage: {
-                width: 55,
-                height: 55
-            },
-            headerImageWrapper: {
-                alignSelf: 'center',
-                flex: 0,
-                padding: 10
-            },
-            headerContainer: {
-                height: 70,
-                backgroundColor: state.theme.colors.surface
-            },
-            headerSubContainer: {
-                flexDirection: 'row',
-                alignItems: 'stretch'
-            },
-            headerText: {
-                flexDirection: 'column',
-                alignItems: 'stretch',
-                flex: 1,
-                padding: 10
-            },
-            headerButton: {
-                alignSelf: 'center',
-                alignItems: 'flex-end',
-                flexDirection: 'row'
-            },
             contentContainer: {
                 height: Dimensions.get("window").height - headerHeight - 165,
                 paddingTop: 10,

dfe0cc5d545d239a7350c58d7a075fa4fe23b410
Marc Steele <marcsteele@Hamish.local>
2020-05-13 10:21:35 +0100

    Test case expansion.

--- components/NowPlayingListItem.js
+++ components/NowPlayingListItem.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Component } from "react";
 import { connect } from "react-redux";
 import { List } from "react-native-paper";
-import { generateStationTheme } from "../branding/branding";
 
 /**
  * List item for displaying the currently on air show.
